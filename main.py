76657e3c7ba21fd3e6122d6ae0a26fcb7aa4e266
LaptopTarks <tano777864@gmail.com>
2024-10-29 21:43:40 -0400

    cambios

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 from flask import Flask, jsonify, request
 
+import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
+
 app = Flask(__name__)
 
 # Simulación de base de datos en memoria
